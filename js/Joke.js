f2e5dbebe28e713c0ca46c70a788d947364a1840
kobe berckmans <kobe.berckmans@student.ehb.be>
2023-03-22 16:20:00 +0100

    progress

---
+++ js/Joke.js
@@ -0,0 +1,10 @@
+export default class Joke {
+    constructor (category, setup, delivery) {
+        this.category = category;
+        this.setup = setup;
+        this.delivery = delivery;
+    }
+    htmlString(){
+        return``
+    }
+    }
--- js/index.js
+++ js/index.js
@@ -5,6 +5,18 @@ const app = {
     init() {
     },
     getData() {
+        fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&amount=10')
+        .then( function (response){
+return response.json();
+        })
+        then(function (json){
+json.jokes.forEach(function (jokeData){
+    const joke = new Joke(jokedata.category, jokeData.setup, jokeData.delivery);
+    app.items.push(joke);
+
+    
+});
+        })
     },
     onSearch() {
     },
